ea01454dd9dd46d98b6ee8ec5e8ab8a0b063fa03
Abdelhamid Birouk <hamid.birouk0597@gmail.com>
2023-02-27 14:56:47 -0500

    assignment edit

--- src/App.js
+++ src/App.js
@@ -6,13 +6,15 @@ import ExpenseTotal from './components/ExpenseTotal';
 import ExpenseList from './components/ExpenseList';
 import AllocationForm from './components/AllocationForm';
 import RemainingBudget from './components/Remaining';
+import Currency from './components/Currency';
+
 const App = () => {
     return (
         <AppProvider>
             <div className='container'>
                 <h1 className='mt-3'>Company's Budget Allocation</h1>
                 <div className='row mt-3'>
-                    <div className='col-sm'>
+                    <div className='col-sm-4'>
                         <Budget />
                     </div>
                     <div className='col-sm'>
@@ -21,13 +23,18 @@ const App = () => {
                     <div className='col-sm'>
                         <ExpenseTotal />
                     </div>
+                    <div className='col-sm'>
+                        <Currency />
+                    </div>
                 </div>
+
                 <h3 className='mt-3'>Allocation</h3>
                 <div className='row '>
                     <div className='col-sm'>
                         <ExpenseList />
                     </div>
                 </div>
+                
                 <h3 className='mt-3'>Change allocation</h3>
                 <div className='row mt-3'>
                     <div className='col-sm'>
--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,11 +1,37 @@
 import React, { useContext } from 'react';
 import { AppContext } from '../context/AppContext';
+
 const Budget = () => {
-    const { budget } = useContext(AppContext);
-    return (
-        <div className='alert alert-secondary'>
-            <span>Budget: £{budget}</span>
-        </div>
-    );
+	const { budget,dispatch,expenses ,Currency} = useContext(AppContext);
+
+	const changeBudget = (val)=>{
+		const totalExpenses = expenses.reduce((total, item) => {
+			return (total += item.cost);
+		}, 0);
+
+		
+		if(val < totalExpenses) {
+			alert("You cannot reduce the budget value lower than the spending!");
+		} else {
+			dispatch({
+				type: 'SET_BUDGET',
+				payload: val,
+			})
+		}
+	}
+	
+	return (
+		<div className='alert alert-secondary'>
+			Budget: {Currency} <input 
+            type="number" 
+            step="10" 
+            style={{ size: 10 }}
+            value={budget} 
+            max='20000'
+            onInput={(event)=>changeBudget(event.target.value)}>
+            </input>
+		</div>
+	);
 };
+
 export default Budget;
